912270acdc80614565ece2934c6d956e1e134fe8
herlanq <herlanq@xavier.edu>
2020-08-31 15:22:11 -0400

    pcb commit

--- phase_1/pcb.c
+++ phase_1/pcb.c
@@ -1,3 +1,8 @@
+/* Quinn Herlan, Kaleb Berry
+ * CSCI 320-01 Operating Systems
+ * Last modified 8/31
+ */
+
 #include ".../h/const.h"
 #include ".../h/types.h"
 #include ".../h/pcb.h"
@@ -20,7 +25,7 @@ typedef struct pcb_t {
     support_t           *p_supportStruct;
                                 /*ptr to support struct*/
 } pcb_t;
-void freePcb(pcb_t *p){
+void freePcb(pcb_PTR p){
 /* Insert the element pointed to by p onto the pcbFree list */
 
 }
@@ -30,15 +35,34 @@ void initPcbs(){
         freePcb(8-foo[i]);
 }
 
-pcb_t* makeEmptyProcQ(){
+pcb_PTR allocPcb();
+
+pcb_PTR makeEmptyProcQ(){
     return null;
 }
 
-int emptyProcQ(pcb_t *tp){
+int emptyProcQ(pcb_PTR tp){
     return (tp==null);
 }
-pcb_t* headProcQ(pcb_t *tp){
+
+void insertProcQ(pbc_PTR *tp, pcb_PTR p);
+
+pcb_PTR removeProcQ(pcb_PTR *tp);
+
+pcb_PTR outProcQ(pcb_PTR *tp, pcb_PTR p);
+
+
+
+pcb_t* headProcQ(pcb_PTR tp){
     if(emptyProcQ(tp))
         return null;
     return(tp->p_next);
 }
+
+int emptyChild(pcb_PTR p);
+
+void insertChild(pcb_PTR prnt, pcb_PTR p);
+
+pcb_PTR removeChild(pcb_PTR p);
+
+pcb_PTR outChild(pcb_PTR p);
