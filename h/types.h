9a0908e3477e3d357e8178272ada244f3d3be85a
herlanq <herlanq@xavier.edu>
2020-09-03 16:42:36 -0400

    pcb commit, pcd type moved to types.h

--- h/types.h
+++ h/types.h
@@ -23,6 +23,25 @@ typedef struct {
 	unsigned int d_data1;
 } device_t;
 
+/* process control block type */
+typedef struct pcb_t {
+    /* process queue fields */
+    struct pcb_t        *p_next,     /*pointer to next entry*/
+                        *p_prev,     /*pointer to prev entry*/
+    /* process tree fields */
+                        *p_print,    /*pointer to parent*/
+                        *p_child,    /*pointer to child*/
+                        *p_sib;      /*pointer to sibling*/
+    /* process status information */
+    state_t             p_s;        /*processor state*/
+    cpu_t               p_time;     /*cpu time used by proc*/
+    int                 *p_semAdd;   /*which process blocked*/
+
+    /* support layer information */
+    support_t           *p_supportStruct;
+                                /*ptr to support struct*/
+} pcb_t;
+
 #define t_recv_status		d_status
 #define t_recv_command		d_command
 #define t_transm_status		d_data0
--- phase_1/pcb.c
+++ phase_1/pcb.c
@@ -7,27 +7,6 @@
 #include ".../h/types.h"
 #include ".../h/pcb.h"
 
-
-
-/* process control block type */
-typedef struct pcb_t {
-    /* process queue fields */
-    struct pcb_t        *p_next,     /*pointer to next entry*/
-                        *p_prev,     /*pointer to prev entry*/
-    /* process tree fields */
-                        *p_print,    /*pointer to parent*/
-                        *p_child,    /*pointer to child*/
-                        *p_sib;      /*pointer to sibling*/
-    /* process status information */
-    state_t             p_s;        /*processor state*/
-    cpu_t               p_time;     /*cpu time used by proc*/
-    int                 *p_semAdd;   /*which process blocked*/
-
-    /* support layer information */
-    support_t           *p_supportStruct;
-                                /*ptr to support struct*/
-} pcb_t;
-
 HIDDEN pcb_t p_head;
 HIDDEN pcb_t p_Lsib;        //hidden pointers used during the tree and stack functions
 HIDDIN pcb_t p_Rsib;
