bfc6ba4e50bd4e732acb331958bfdc1a7958b02b
Kaleb Berry <46752783+k98berry@users.noreply.github.com>
2020-09-04 14:18:15 -0400

    Update types.h
    added the previous sibling to the types file.

--- h/types.h
+++ h/types.h
@@ -32,6 +32,7 @@ typedef struct pcb_t {
                         *p_print,    /*pointer to parent*/
                         *p_child,    /*pointer to child*/
                         *p_sib;      /*pointer to sibling*/
+			*p_prevsib;  /*pointer to previous sib*/
     /* process status information */
     state_t             p_s;        /*processor state*/
     cpu_t               p_time;     /*cpu time used by proc*/
