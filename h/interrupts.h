10a8feee00b0632fe55afbcb152b1f9d0168950b
herlanq <herlanq@xavier.edu>
2020-10-13 14:35:09 -0700

    interrupts

--- h/interrupts.h
+++ h/interrupts.h
@@ -4,7 +4,7 @@
 #define INTERRUPTS
 
 extern void InterruptHandler();
-void Device_InterruptH(int line);
+HIDDEN void Device_InterruptH(int line);
 extern void Copy_Paste(state_t *copied_state, state_t *pasted_state);
 
 #endif
