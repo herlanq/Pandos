1ae9d10810b4f5ffe47b416c8ee25a03c42739f4
herlanq <herlanq@xavier.edu>
2020-10-20 16:20:46 -0700

    cleaned up initial.c

--- phase_2/initial.c
+++ phase_2/initial.c
@@ -19,7 +19,10 @@
 #include "../h/scheduler.h"
 
 #include "../h/libumps.h"
-
+/* Internal and External Function Declaration*/
+extern void test(); /* declares test() from the p2test file */
+HIDDEN void genExceptionHandler();
+extern void uTLB_RefillHandler();
 
 /* Global Variables */
 int processCount; /* number of processes on the ready queue */
@@ -28,50 +31,50 @@ pcb_t *currentProc; /* Pointer to current process */
 pcb_t *readyQue; /* pointer to the ready queue */
 cpu_t start_clock;
 cpu_t sliceCNT;
-unsigned int device_status[SEMNUM-1]; /* save device state area */
-int semD[SEMNUM]; /* 49 Semaphore in the list */
+int semD[SEMNUM]; /* 49 Semaphore in the list, [0 - 48] */
+#define CLOCKSEM semD[SEMNUM-1] /* clock sema4 in the device sema4 list , == semD[48] */
+
+/* debug variables */
 int exception_check;
-#define CLOCKSEM semD[SEMNUM-1]
 
-
-/* declares test() from the p2test file */
-extern void test();
-HIDDEN void genExceptionHandler();
-extern void uTLB_RefillHandler();
 
 /* This is the starting point, the main, of the OS. This initializes variables, sets memory addresses,
  * and declares variables that will be used throughout the phase 2 modules.
  * One the main is complete, it passes over to the scheduler */
 int main(){
-    /* init global variables */
+    /* init local variables */
     processCount = 0;
     softBlockCount = 0;
-    currentProc = NULL;
-    readyQue = mkEmptyProcQ();
-    /* init semaphores */
-    int i;
-    for(i=0; i < SEMNUM; i++){
-        semD[i] = 0;
-    }
+    pcb_PTR proc;
 
+    /* initializing the pass up vector */
     passupvector_t *passupvector = (passupvector_t*) PASSUPVECTOR;
     passupvector->tlb_refll_handler = (memaddr) uTLB_RefillHandler;
     passupvector->tlb_refll_stackPtr = (memaddr) PASSUPVECTOR + 0x04;
     passupvector->execption_handler = (memaddr) genExceptionHandler;
     passupvector->exception_stackPtr = (memaddr) PASSUPVECTOR + 0x04;
-    
-
 
     /* Init pcb and asl */
     initPcbs();
     initASL();
+    /* prepare empty readyque and current process */
+    currentProc = NULL;
+    readyQue = mkEmptyProcQ();
+
+    /* init semaphores on the device sema4 list */
+    int i;
+    for(i=0; i < (SEMNUM-1); i++){
+        semD[i] = 0;
+    }
+
+    /*set the clock semaphore to 0 */
+    CLOCKSEM = 0;
 
     /* load time onto the pseudo clock */
     LDIT(PSUEDOCLOCKTIME);
-    CLOCKSEM = 0;
 
     /* alloc process to be set the current process, increment procCount */
-    pcb_PTR proc = allocPcb();
+    proc = allocPcb();
     if(proc != NULL) {
         processCount = processCount + 1;
         memaddr ramtop = *(int*)RAMBASEADDR + *(int*)RAMBASESIZE;
@@ -79,15 +82,11 @@ int main(){
         proc->p_s.s_pc = (memaddr) test;
         proc->p_s.s_t9 = (memaddr) test;
         proc->p_s.s_status = (ALLOFF | IECON | IMON | TEBITON);
-        
 
         /* insert current proc into the ready queue*/
         insertProcQ(&readyQue, proc);
 
-        /* init current proc back to NULL */
-        currentProc = NULL;
-
-        /* Scheduler takes over the running process */
+        /* Call the Scheduler for the next process to take over */
         scheduler();
     }else{
         PANIC();
@@ -95,12 +94,15 @@ int main(){
     return(0);
 }
 
+/* Calls the appropriate exception handler based on the cause */
 void genExceptionHandler(){
-/*turning off the bits we don't need, and then shifting them over to make a comparison */
     int eReason;
     state_PTR oldState = (state_PTR) BIOSDATAPAGE;
-    if(currentProc != NULL)
+
+    /* if the current process != NULL, copy all of its register values and paste them into a new state */
+    if(currentProc != NULL){
         Copy_Paste(oldState, &currentProc->p_s);
+    }
     eReason = (oldState->s_cause & CAUSE) >> SHIFT;
     exception_check = eReason;
         if(eReason == IOINTERRUPT){
