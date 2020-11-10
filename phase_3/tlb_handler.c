ffa9ec3d0b0579a689e8ba1c2a9566aa97853aa3
herlanq <herlanq@xavier.edu>
2020-11-10 15:06:38 -0800

    started phase 3

--- phase_3/tlb_handler.c
+++ phase_3/tlb_handler.c
@@ -17,24 +17,45 @@ Each Page Table entry is a doubleword consisting of an EntryHi and an EntryLo po
 #include "../h/scheduler.h"
 #include "../h/libumps.h"
 
-
+extern pcb_t *currentProc;
+swap_t swap_pool[POOLSIZE];
+int swap_sem = 1;
+/*Planning on using this function to initialize all the structures needed for each process,
+possibly the swap pool and backing store as well */
 void initialStructs(){
-	/*Planning on using this function to initialize all the structures needed for each process,
-	possibly the swap pool and backing store as well */
+    for(int i=0; i < POOLSIZE; i++){
+        swap_pool[i].sw_asid = -1;
+    }
 }
 
+/*this function is used for TLB invalid and modification exceptions,
+it should check to make sure that the D-bit is on, and also check the valid bit. */
 void uTLB_exceptionHandler(){
-	/*this function is used for TLB invalid and modification exceptions,
-	it should check to make sure that the D-bit is on, and also check the valid bit. */
+
 }
 
+/*This function is used for when there is no TLB entry fould,
+this function goes and searches for it within the page table */
 void uTLB_RefillHandler(/*I think the ASID is given to us */){
-	/*This function is used for when there is no TLB entry fould,
-	 this function goes and searches for it within the page table */
+    state_PTR oldstate;
+    int missing_num;
+    oldstate = (state_PTR) BIOSDATAPAGE;
+    missing_num = (oldstate->s_entryHI & GETPAGENUM) >> VPNSHIFT;
+    missing_num = missing_num % MAXPAGES;
+    setENTRYHI((currentProc->p_supportStruct->sup_PvtPgTable[missing_num]).entryHI);
+    setENTRYLO((currentProc->p_supportStruct->sup_PvtPgTable[missing_num]).entryLO);
+    TLBWR();
+    LDST(oldstate);
+
+    uVM_Hanlder();
+
+    SYSCALL(8,0,0,0);
+
 	for(int i = 0, i<= (unsigned int) RAMTOP, i++){ /*go through the page table to see where the TLB entry is */
 		if(ASID == given_ASID){
 			/*might have to check the valid bit as well but yeah */
 			/*write it into the TLB and LDST on currentproc*/
-		} 
+		}
 	}
+
 }
