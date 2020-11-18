6a55ee599ca24ac6a88548969a966585e2b75ebf
k98berry <berryk6@xavier.edu>
2020-11-17 16:23:47 -0800

    workin towards clean compile

--- phase_3/syssupport.c
+++ phase_3/syssupport.c
@@ -10,10 +10,10 @@ Written by Kaleb Berry and Quinn Herlan */
 
 /*this function is used to pull the support struct, check the exception, then determine what syscall to perform */
 void SysSupport(){
-	support_t supportStruct;
+	support_t* supportStruct;
 	int cause;
 	/*first things first, get the support struct */
-	supportStruct = SYSCALL(GETSPTPTR,0,0,0);
+	supportStruct = (support_t*) SYSCALL(GETSPTPTR,0,0,0);
 	supportStruct->sup_exceptState[GENERALEXCEPT].s_pc += 4;
 	cause = (supportStruct->sup_exceptState[GENERALEXCEPT].s_cause & CAUSE) >> SHIFT;
 
@@ -21,22 +21,20 @@ void SysSupport(){
 		uSysHandler(supportStruct);
 
 	/*I think that we just terminate here? not sure what situation this puts us in */
-	SYSCALL(TERMINATEPROCESS,0,0,0);
+	SYSCALL(TERMINATETHREAD,0,0,0);
 }
 
 void uSysHandler(support_t *supportStruct){
-    cpu_t time;
+    
 	int sysReason = supportStruct->sup_exceptState[GENERALEXCEPT].s_a0;
-
 	if(sysReason == TERMINATE){
 		/*this is the case where we terminate process */
-		SYSCALL(TERMINATEPROCESS,0,0,0);
-		break;
+		SYSCALL(TERMINATETHREAD,0,0,0);
 	}
 	else if(sysReason == GETTOD){
+		cpu_t time;
 		STCK(time);
 		supportStruct->sup_exceptState[GENERALEXCEPT].s_v0 = time;
-		break;
 	}
 	else if(sysReason == PRINTERW){
 		/*this is the case where we write to printer */
@@ -45,12 +43,12 @@ void uSysHandler(support_t *supportStruct){
 		int devSem; /*used to determine the device semapore */
 		int length; /*used to determine the length of output */
 
-		char charAddress;
-		devregarea_t devReg; /*device register type */
+		char* charAddress;
+		devregarea_t* devReg; /*device register type */
 		id = supportStruct->sup_asid;
-		devReg = (devregarea_t *) RAMBASEADDR;
+		devReg = (devregarea_t*) RAMBASEADDR;
 		devSem = ((PRINTERINT - DISKINT) * DEVPERINT) + (id -1);
-		charAddress = supportStruct->sup_exceptState[GENERALEXCEPT].s_a1;
+		charAddress =(char*) supportStruct->sup_exceptState[GENERALEXCEPT].s_a1;
 		length = supportStruct->sup_exceptState[GENERALEXCEPT].s_a2;
 
 		int counter = 0; /*used for the while loop */
@@ -61,7 +59,7 @@ void uSysHandler(support_t *supportStruct){
 			status = SYSCALL(WAITIO, PRINTER, (id-1), 0);
 			if(status != READY){
 				supportStruct->sup_exceptState[GENERALEXCEPT].s_v0 = (status *-1);
-				status == status * -1;
+				status = status * -1;
 			}
 			charAddress++;
 			counter++;
@@ -76,23 +74,23 @@ void uSysHandler(support_t *supportStruct){
 		int devSem; /*used to determine the device semapore */
 		int length; /*used to determine the length of output */
 
-		memaddr* charAddress;
-		devregarea_t devReg; /*device register type */
+		char* charAddress;
+		devregarea_t* devReg; /*device register type */
 		id = supportStruct->sup_asid;
-		devReg = (devregarea_t *) RAMBASEADDR;
+		devReg = (devregarea_t*) RAMBASEADDR;
 		devSem = ((PRINTERINT - DISKINT) * DEVPERINT) + (id -1);
-		charAddress = supportStruct->sup_exceptState[GENERALEXCEPT].s_a1;
+		charAddress =(char*) supportStruct->sup_exceptState[GENERALEXCEPT].s_a1;
 		length = supportStruct->sup_exceptState[GENERALEXCEPT].s_a2;
 
 		int counter; /*used for the while loop */
 		while((status == READY) && (counter < length)){
 			/*need to work on the output for this function */
 			devReg->devreg[devSem].d_data0 = charAddress;
-			devReg->devreg[devSem].d_command = /*the transmat constant */ ;
+			devReg->devreg[devSem].d_command = 2;
 			status = SYSCALL(WAITIO, PRINTER, (id-1), 0);
 			if(status != READY){
 				supportStruct->sup_exceptState[GENERALEXCEPT].s_v0 = (status *-1);
-				status == status * -1;
+				status = status * -1;
 			}
 			charAddress++;
 			counter++;
@@ -105,7 +103,7 @@ void uSysHandler(support_t *supportStruct){
 	}
 
 	else{
-		SYSCALL(TERMINATEPROCESS,0,0,0);
+		SYSCALL(TERMINATETHREAD,0,0,0);
 	}
 
 }
