cd31cbd77fad2df57cd23fdd0ed8d80d7d7e94d5
herlanq <herlanq@xavier.edu>
2020-11-18 15:41:49 -0800

    cleaned up and changed some things

--- phase_3/syssupport.c
+++ phase_3/syssupport.c
@@ -15,88 +15,128 @@ void SysSupport(){
 	/*first things first, get the support struct */
 	supportStruct = (support_t*) SYSCALL(GETSPTPTR,0,0,0);
 	supportStruct->sup_exceptState[GENERALEXCEPT].s_pc += 4;
+	/* get exception cause */
 	cause = (supportStruct->sup_exceptState[GENERALEXCEPT].s_cause & CAUSE) >> SHIFT;
 
-	if(cause == SYSEXCEPTION)
-		uSysHandler(supportStruct);
+	/* If Syscall */
+	if(cause == SYSEXCEPTION) {
+        uSysHandler(supportStruct);
+    }
 
-	/*I think that we just terminate here? not sure what situation this puts us in */
+	/* Else, (for our purposes) Terminate the process */
 	SYSCALL(TERMINATETHREAD,0,0,0);
 }
 
 void uSysHandler(support_t *supportStruct){
     
 	int sysReason = supportStruct->sup_exceptState[GENERALEXCEPT].s_a0;
+
+    /* Begin Terminate Case */
 	if(sysReason == TERMINATE){
 		/*this is the case where we terminate process */
 		SYSCALL(TERMINATETHREAD,0,0,0);
-	}
+	} /* End Terminate Case */
+
+	/* Begin Get TOD Case */
 	else if(sysReason == GETTOD){
 		cpu_t time;
 		STCK(time);
 		supportStruct->sup_exceptState[GENERALEXCEPT].s_v0 = time;
-	}
+	} /* End Get TOD Case */
+
+	/* Begin Write to Printer Case */
 	else if(sysReason == PRINTERW){
 		/*this is the case where we write to printer */
 		int id; /*this is the asid of the process */
 		int status; /*used for writing to printer and terminal */
-		int devSem; /*used to determine the device semapore */
+		int error;
+		int devSemNum; /*used to determine the device semapore */
 		int length; /*used to determine the length of output */
+		char *charAddress;
+		devregarea_t *devReg; /*device register type */
 
-		char* charAddress;
-		devregarea_t* devReg; /*device register type */
 		id = supportStruct->sup_asid;
 		devReg = (devregarea_t*) RAMBASEADDR;
-		devSem = ((PRINTERINT - DISKINT) * DEVPERINT) + (id -1);
-		charAddress =(char*) supportStruct->sup_exceptState[GENERALEXCEPT].s_a1;
+        devSemNum = ((PRINTER - DISK) * DEVPERINT) + (id-1);
+
+		charAddress = (char*) supportStruct->sup_exceptState[GENERALEXCEPT].s_a1;
 		length = supportStruct->sup_exceptState[GENERALEXCEPT].s_a2;
 
+		if(((int)charAddress < KUSEG) || (length < 1)){
+		    SYSCALL(TERMINATETHREAD, 0, 0, 0);
+		}
+
+		/* P the semaphore and get mutual exclusion */
+		SYSCALL(PASSERN, devSem[devSemNum], 0, 0);
+
 		int counter = 0; /*used for the while loop */
-		while((status == READY) && (counter < length)){
+		error = FALSE;
+		while((!error) && (counter < length)){
 			/*need to work on the output for this function */
-			devReg->devreg[devSem].d_data0 = charAddress;
-			devReg->devreg[devSem].d_command = 2;
+			devReg->devreg[devSemNum].d_data0 = (*charAddress);
+			devReg->devreg[devSemNum].d_command = 2;
 			status = SYSCALL(WAITIO, PRINTER, (id-1), 0);
 			if(status != READY){
-				supportStruct->sup_exceptState[GENERALEXCEPT].s_v0 = (status *-1);
-				status = status * -1;
+			    error = TRUE;
+				/*supportStruct->sup_exceptState[GENERALEXCEPT].s_v0 = (status *-1);*/
+			}else{
+                counter++;
 			}
-			charAddress++;
-			counter++;
+            charAddress++;
 		}
-		supportStruct->sup_exceptState[GENERALEXCEPT].s_v0 = counter;
-	}
+		/* V the semaphore and release mutual exclusion */
+		SYSCALL(VERHOGEN, devSem[devSemNum], 0, 0);
 
+		/* assign the number of characters to the process */
+		supportStruct->sup_exceptState[GENERALEXCEPT].s_v0 = counter;
+	} /* End Write to Printer Case */
+
+	/* Begin Terminal Write Case */
 	else if(sysReason == TERMINALW){
 		/*this is the case where we write to terminal */
 		int id; /*this is the asid of the process */
 		int status; /*used for writing to printer and terminal */
-		int devSem; /*used to determine the device semapore */
+		int devSemNum; /*used to determine the device semapore */
+		int error;
 		int length; /*used to determine the length of output */
-
 		char* charAddress;
 		devregarea_t* devReg; /*device register type */
+
 		id = supportStruct->sup_asid;
 		devReg = (devregarea_t*) RAMBASEADDR;
-		devSem = ((PRINTERINT - DISKINT) * DEVPERINT) + (id -1);
+        devSemNum = ((PRINTER - DISK) * DEVPERINT) + (id-1);
 		charAddress =(char*) supportStruct->sup_exceptState[GENERALEXCEPT].s_a1;
 		length = supportStruct->sup_exceptState[GENERALEXCEPT].s_a2;
 
-		int counter; /*used for the while loop */
-		while((status == READY) && (counter < length)){
-			/*need to work on the output for this function */
-			devReg->devreg[devSem].d_data0 = charAddress;
-			devReg->devreg[devSem].d_command = 2;
+        if(((int)charAddress < KUSEG) || (length < 1)){
+            SYSCALL(TERMINATETHREAD, 0, 0, 0);
+        }
+        /* P the semaphore and get mutual exclusion */
+        SYSCALL(PASSERN, devSem[devSemNum], 0, 0);
+
+        int counter = 0; /*used for the while loop */
+        error = FALSE;
+		while((!error) && (counter < length)){
+		    devReg->devreg[devSemNum].t_transm_command = *charAddress;
+
+			/* Sys 8 the terminal write */
 			status = SYSCALL(WAITIO, PRINTER, (id-1), 0);
-			if(status != READY){
-				supportStruct->sup_exceptState[GENERALEXCEPT].s_v0 = (status *-1);
-				status = status * -1;
+
+			if((status & 0xFF) != READY){
+			    error = TRUE;
 			}
 			charAddress++;
 			counter++;
 		}
+		/* V the semaphore to release mutual exclusion */
+		SYSCALL(VERHOGEN, devSem[devSemNum], 0, 0);
+
+		if(error){
+		    counter = 0 - (status&0xFF);
+		}
+
 		supportStruct->sup_exceptState[GENERALEXCEPT].s_v0 = counter;
-	}
+	} /* End Terminal Write Case */
 
 	else if(sysReason == TERMINALR){
 		/*this is the case where we read from terminal, havnt even started this yet */
